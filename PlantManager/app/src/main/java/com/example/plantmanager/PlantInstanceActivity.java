9f88747aacaf8e303d13003d0465a51525d45cff
Remus Courtenay <remuscourtenay@gmail.com>
2021-04-09 16:40:06 +1200

    Added empty activity for plant instance

--- PlantManager/app/src/main/java/com/example/plantmanager/PlantInstanceActivity.java
+++ PlantManager/app/src/main/java/com/example/plantmanager/PlantInstanceActivity.java
@@ -1,16 +1,15 @@
 package com.example.plantmanager;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PlantInstanceActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_plant_instance);
     }
-
 
 }
