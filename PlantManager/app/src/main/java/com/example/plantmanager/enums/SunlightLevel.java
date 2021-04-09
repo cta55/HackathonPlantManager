097d569a9b268193b80475879d14cd96c7738529
Remus Courtenay <remuscourtenay@gmail.com>
2021-04-09 21:46:50 +1200

    Added images to the Sunlight level enum

--- PlantManager/app/src/main/java/com/example/plantmanager/enums/SunlightLevel.java
+++ PlantManager/app/src/main/java/com/example/plantmanager/enums/SunlightLevel.java
@@ -1,9 +1,25 @@
 package com.example.plantmanager.enums;
 
+import com.example.plantmanager.R;
+
+/**
+ * Basic enum for possible values of sunlight that a plant may need. Each level comes linked with
+ * an image from the resources mipmap folder.
+ */
 public enum SunlightLevel {
-    NO_SUNLIGHT,
-    LOW_SUNLIGHT,
-    MEDIUM_SUNLIGHT,
-    HIGH_SUNLIGHT,
-    FULL_SUNLIGHT;
+    NO_SUNLIGHT(R.mipmap.ic_no_sunlight),
+    LOW_SUNLIGHT(R.mipmap.ic_low_sunlight),
+    MEDIUM_SUNLIGHT(R.mipmap.ic_medium_sunlight),
+    HIGH_SUNLIGHT(R.mipmap.ic_high_sunlight),
+    FULL_SUNLIGHT(R.mipmap.ic_full_sunlight);
+
+    private final int imageID;
+
+    SunlightLevel(int imageID) {
+        this.imageID = imageID;
+    }
+
+    public int getImageID() {
+        return imageID;
+    }
 }
