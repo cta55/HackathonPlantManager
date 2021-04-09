59eba61e437ca2106a78ee9122d1fa11068b8593
Remus Courtenay <remuscourtenay@gmail.com>
2021-04-09 17:37:20 +1200

    Filled in header fragment

--- PlantManager/app/src/main/java/com/example/plantmanager/HeaderFragment.java
+++ PlantManager/app/src/main/java/com/example/plantmanager/HeaderFragment.java
@@ -7,58 +7,76 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link HeaderFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * Simple fragment that handles the placement of a text header
  */
+
 public class HeaderFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    // String key for passing in header text
+    private static final String HEADER_TEXT_CODE = "header_text";
+    // Default value, shouldn't appear in finished product
+    private static final String DEFAULT_HEADER_TEXT = "DEFAULT_TEXT";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // Inputted text to show in header
+    private String headerText;
+    // TextView that exists in header layout
+    private TextView headerTextView;
 
-    public HeaderFragment() {
-        // Required empty public constructor
+    /**
+     * Private basic constructor used in the static method.
+     */
+    private HeaderFragment() {
     }
 
     /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HeaderFragment.
+     * Static method used to instantiate new header fragments with a specific value for header text
+     * @param headerText : The text to appear in the header
+     * @return : A HeaderFragment object bundled with a specific header text
      */
-    // TODO: Rename and change types and number of parameters
-    public static HeaderFragment newInstance(String param1, String param2) {
+    public static HeaderFragment newHeader(String headerText) {
+        // Instantiating basic object
         HeaderFragment fragment = new HeaderFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        // Adding inputted headerText value into object
+        args.putString(HEADER_TEXT_CODE, headerText);
         fragment.setArguments(args);
+
+        // Passing fragment back out
         return fragment;
     }
 
+    /**
+     * Standard view method that runs when fragment is added to screen. Takes headerText from bundle
+     * and saves it into the headerText field.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Retrieving headerText value from bundle
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            headerText = savedInstanceState.getString(HEADER_TEXT_CODE);
+        } else {
+            headerText = DEFAULT_HEADER_TEXT;
         }
     }
 
+    /**
+     * Standard fragment method that runs when fragment is added to view. Gets TextView from id and
+     * assigns it the value that was stored in headerText
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_header, container, false);
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_header, null);
+
+        // Getting text view
+        headerTextView = (TextView) root.findViewById(R.id.headerTextView);
+        headerTextView.append(headerText);
+        return root;
     }
 }
