2c7dcd2014864673ac99660f41b95570f39ebb72
Henry Barnett <hbar0010@student.monash.edu>
2021-04-10 15:54:43 +1000

    Removed redundant TypeConverters

--- PlantManager/app/src/main/java/com/example/plantmanager/TypeConverters.java
+++ PlantManager/app/src/main/java/com/example/plantmanager/TypeConverters.java
@@ -6,16 +6,16 @@ import com.example.plantmanager.enums.*;
 public class TypeConverters {
 
 
-    //PlantType Converters
-    @TypeConverter
-    public static PlantTypes stringToPlantType(String databaseValue) {
-        return databaseValue == null ? null : PlantTypes.valueOf(databaseValue);
-    }
-
-    @TypeConverter
-    public static String plantBreedToString(PlantTypes plantType) {
-        return plantType == null ? null : plantType.toString();
-    }
+//    //PlantType Converters
+//    @TypeConverter
+//    public static PlantTypes stringToPlantType(String databaseValue) {
+//        return databaseValue == null ? null : PlantTypes.valueOf(databaseValue);
+//    }
+//
+//    @TypeConverter
+//    public static String plantBreedToString(PlantTypes plantType) {
+//        return plantType == null ? null : plantType.toString();
+//    }
 
     //PlantBreed Converters
     @TypeConverter
@@ -27,28 +27,28 @@ public class TypeConverters {
     public static String plantBreedToString(PlantBreed plantBreed) {
         return plantBreed == null ? null : plantBreed.toString();
     }
-
-    //WaterStage Converters
-    @TypeConverter
-    public static WaterLevel stringToWaterStage(String databaseValue) {
-        return databaseValue == null ? null : WaterLevel.valueOf(databaseValue);
-    }
-
-    @TypeConverter
-    public static String waterStageToString(WaterLevel waterLevel) {
-        return waterLevel == null ? null : waterLevel.toString();
-    }
-
-    //SunlightLevel Converters
-    @TypeConverter
-    public static SunlightLevel stringToSunlightLevel(String databaseValue) {
-        return databaseValue == null ? null : SunlightLevel.valueOf(databaseValue);
-    }
-
-    @TypeConverter
-    public static String sunlightLevelToString(SunlightLevel sunlightLevel) {
-        return sunlightLevel == null ? null : sunlightLevel.toString();
-    }
+//
+//    //WaterStage Converters
+//    @TypeConverter
+//    public static WaterLevel stringToWaterStage(String databaseValue) {
+//        return databaseValue == null ? null : WaterLevel.valueOf(databaseValue);
+//    }
+//
+//    @TypeConverter
+//    public static String waterStageToString(WaterLevel waterLevel) {
+//        return waterLevel == null ? null : waterLevel.toString();
+//    }
+//
+//    //SunlightLevel Converters
+//    @TypeConverter
+//    public static SunlightLevel stringToSunlightLevel(String databaseValue) {
+//        return databaseValue == null ? null : SunlightLevel.valueOf(databaseValue);
+//    }
+//
+//    @TypeConverter
+//    public static String sunlightLevelToString(SunlightLevel sunlightLevel) {
+//        return sunlightLevel == null ? null : sunlightLevel.toString();
+//    }
 
 
 }
