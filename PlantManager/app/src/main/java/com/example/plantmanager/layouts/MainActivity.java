6c7fcb5f7c25a1582c456b63dc3bf81a9957e52d
Cam <cta67@uclive.ac.nz>
2021-04-10 17:25:18 +1200

    some home screen buttons

--- PlantManager/app/src/main/java/com/example/plantmanager/layouts/MainActivity.java
+++ PlantManager/app/src/main/java/com/example/plantmanager/layouts/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.plantmanager.layouts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.plantmanager.PlantDB;
 import com.example.plantmanager.R;
@@ -14,5 +17,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button myPlantsButton = findViewById(R.id.MyPlantsButton);
+        Button plantsDatabaseButton = findViewById(R.id.PlantDatabaseButton);
+
+        myPlantsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+//                Intent myPlantsIntent = new Intent(this, );
+//                startActivity(myPlantsIntent);
+            }
+        });
+
+        plantsDatabaseButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent plantsSearchIntent = new Intent(v.getContext(), PlantTypeListActivity.class);
+                startActivity(plantsSearchIntent);
+            }
+        });
     }
 }
