0e6da58dc3e86e22a441af207544323b56da95c7
Remus Courtenay <remuscourtenay@gmail.com>
2021-04-11 14:54:51 +1200

    first version adapter

--- PlantManager/app/src/main/java/com/example/plantmanager/layouts/fragments/RecyclerFragment.java
+++ PlantManager/app/src/main/java/com/example/plantmanager/layouts/fragments/RecyclerFragment.java
@@ -1,6 +1,32 @@
 package com.example.plantmanager.layouts.fragments;
 
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.plantmanager.viewModels.PlantImageFragmentViewModel;
+
+import java.util.List;
 
 public class RecyclerFragment extends Fragment {
+
+    PlantImageFragmentViewModel viewModel;
+    List<PlantInstancePictureFragment> plantInstancePictureFragments;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        viewModel = new ViewModelProvider(requireActivity()).get(PlantImageFragmentViewModel.class);
+        plantInstancePictureFragments = viewModel.getList();
+    }
+
 }
---
+++ PlantManager/app/src/main/java/com/example/plantmanager/layouts/fragments/plantPictureAdapter.java
@@ -0,0 +1,60 @@
+package com.example.plantmanager.layouts.fragments;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.plantmanager.R;
+
+import java.util.List;
+
+public class PlantPictureAdapter extends RecyclerView.Adapter<PlantPictureAdapter.ViewHolder>{
+
+    private final List<PlantInstancePictureFragment> fragmentList;
+    private final FragmentManager manager;
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final FragmentContainerView containerView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            containerView = (FragmentContainerView) itemView.findViewById(R.id.plantInstanceImageFragmentContainerView);
+        }
+
+        public FragmentContainerView getContainerView() {
+            return containerView;
+        }
+    }
+
+    public PlantPictureAdapter(List<PlantInstancePictureFragment> fragmentList, FragmentManager manager) {
+        this.fragmentList = fragmentList;
+        this.manager = manager;
+    }
+
+
+    @NonNull
+    @Override
+    public PlantPictureAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.plant_image_fragment_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PlantPictureAdapter.ViewHolder holder, int position) {
+        manager.beginTransaction()
+                .setReorderingAllowed(true)
+                .add(holder.containerView.getId(), fragmentList.get(position))
+                .commit();
+    }
+
+    @Override
+    public int getItemCount() {
+        return fragmentList.size();
+    }
+}
